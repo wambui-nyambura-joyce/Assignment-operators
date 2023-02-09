7a1f4418a2c81f7245551adf741134f04384ea3e
wambui-nyambura <nyamburawambuui@gmail.com>
2023-02-09 16:13:24 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+name()
+    add()
+    sum(65, 82, 74, 21)
+    fact()
+}
+fun name (){
+    var name = "Nyambura"
+    println("Hi" + " " + name)
+}
+fun add (){
+    var y = 43
+    var x = 76
+    var sum = y % x
+    println(sum)
+}
+fun sum (num1:Int, num2:Int, num3:Int, num4:Int ){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+}
+fun fact (){
+    var fact= "I am a student at AkiraChix"
+    println(fact)
+
+}
